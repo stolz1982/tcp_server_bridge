5b2d0c8d0492b8a139c5fa5ec58555f8d4edaccd
stolz1982 <stolz1982@gmx.de>
2018-10-18 15:11:39 +0200

    created the service file

--- tcp_server_bridge_install.sh
+++ tcp_server_bridge_install.sh
@@ -1,32 +1,27 @@
 #!/bin/bash
 source ./tcp_server_bridge.cfg
 
+echo "Creating the service file"
+rm -f $TCP_SERVER_BRIDGE_SERVICE
+touch $TCP_SERVER_BRIDGE_SERVICE
 
-
-
-
-
-echo "Creating the user file"
-rm -rf ./tcp_server_bridge.service
-touch ./
-
-echo "[Unit]" >> 
-Description=Tevin TCP Server Bridge
-After=syslog.target
-
-[Service]
-Type=simple
-User=$TCP_SERVER_BRIDGE_USER
-Group=$TCP_SERVER_BRIDGE_USER
-WorkingDirectory=$TCP_SERVER_BRIDGE_DIR
-ExecStart=$TCP_SERVER_BRIDGE_DIR/$TCP_SERVER_BRIDGE_FILE
-SyslogIdentifier=$SYSLOGID
-StandardOutput=syslog
-StandardError=syslog
-Restart=always
-RestartSec=3
-
-[Install]
-WantedBy=multi-user.target 
+echo "[Unit]" >> $TCP_SERVER_BRIDGE_SERVICE
+echo "Description=Tevin TCP Server Bridge" >> $TCP_SERVER_BRIDGE_SERVICE
+echo "After=syslog.target" >> $TCP_SERVER_BRIDGE_SERVICE
+echo "" >> $TCP_SERVER_BRIDGE_SERVICE
+echo "[Service]" >> $TCP_SERVER_BRIDGE_SERVICE
+echo "Type=simple" >> $TCP_SERVER_BRIDGE_SERVICE
+echo "User=$TCP_SERVER_BRIDGE_USER" >> $TCP_SERVER_BRIDGE_SERVICE
+echo "Group=$TCP_SERVER_BRIDGE_USER" >> $TCP_SERVER_BRIDGE_SERVICE
+echo "WorkingDirectory=$TCP_SERVER_BRIDGE_DIR" >> $TCP_SERVER_BRIDGE_SERVICE
+echo "ExecStart=$TCP_SERVER_BRIDGE_DIR/$TCP_SERVER_BRIDGE_FILE" >> $TCP_SERVER_BRIDGE_SERVICE
+echo "SyslogIdentifier=$SYSLOGID" >> $TCP_SERVER_BRIDGE_SERVICE
+echo "StandardOutput=syslog" >> $TCP_SERVER_BRIDGE_SERVICE
+echo "StandardError=syslog" >> $TCP_SERVER_BRIDGE_SERVICE
+echo "Restart=always" >> $TCP_SERVER_BRIDGE_SERVICE
+echo "RestartSec=3" >> $TCP_SERVER_BRIDGE_SERVICE
+echo "" >> $TCP_SERVER_BRIDGE_SERVICE
+echo "[Install]" >> $TCP_SERVER_BRIDGE_SERVICE
+echo "WantedBy=multi-user.target" >> $TCP_SERVER_BRIDGE_SERVICE
 
 
